9e3e86ce95a04ca855f44d34d42cda77f93cbae7
freddie-yyh <freddie.yyh@kakaocorp.com>
2019-04-23 19:47:15 +0900

    google api key 제거

--- src/stories/GoogleLoginButton.stories.js
+++ src/stories/GoogleLoginButton.stories.js
@@ -6,5 +6,5 @@ import GoogleLoginButton from '../components/GoogleLoginButton.vue';
 storiesOf('GoogleLoginButton', module)
   .add('Basic', () => ({
     components: { GoogleLoginButton },
-    template: '<google-login-button appkey="AIzaSyD2Udfhf3MxYxtb19lRDARBD1v_JCbBH6w"></google-login-button>',
+    template: '<google-login-button appkey=""></google-login-button>',
   }));
